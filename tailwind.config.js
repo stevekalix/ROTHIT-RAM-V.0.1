afaf39ad45f41af97574a04d9e0eb7429f633511
stevekalix <msr962005@gmail.com>
2026-02-20 21:06:07 +0530

    base update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    base:process.env.VITE_BASH_PATH || "/",
+    
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
